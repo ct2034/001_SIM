c04582ef89a91c838918fa9f16a04818a3cbc8a9
cch <christian.henkel@ipa.fraunhofer.de>
2017-02-22 19:48:45 +0100

    z

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-#Klassischer Aufbau: Imports, Definitionen, Ausführung
+# Klassischer Aufbau: Imports, Definitionen, Ausführung
 from time import clock
 from time import sleep
 from numpy import linalg as LA
-#Eigene importe
+# Eigene importe
 from Station import Station
 from Product import Product
 import threading
@@ -21,19 +21,19 @@ class Transport_Handler(object):
         self.n_products = n_products;
         self.lade_Listen();
 
-    def start(self,n=10):
+    def start(self, n=10):
         print("Simulation startet:")
         t_start = clock()
 
-        for i in range(0,n):
-            t = threading.Thread(target=Product, args=(self,self.stations,self.flow,i)).start();
-            #self.product_list.append(t);
-        sleep(0.1*n);
+        for i in range(0, n):
+            t = threading.Thread(target=Product, args=(self, self.stations, self.flow, i)).start();
+            # self.product_list.append(t);
+        sleep(0.1 * n);
 
-        #print("Product :",self.product_list)
+        # print("Product :",self.product_list)
         are_products_finished = False;
         while (are_products_finished == False):
-            is_done=True;
+            is_done = True;
             for prod in self.product_list:
                 if (not prod.is_finished()):
                     is_done = False;
@@ -44,11 +44,11 @@ class Transport_Handler(object):
 
         t_end = clock()
         t_dt = t_end - t_start
-        print("Der Vorgang mit",n,"Produkten dauerte",t_dt,"sekunden.")
+        print("Der Vorgang mit", n, "Produkten dauerte", t_dt, "sekunden.")
 
-    def register_products(self,prod=Product):
+    def register_products(self, prod=Product):
         self.product_list.append(prod);
-        #print("Product was added, size:",self.product_list.__len__());
+        # print("Product was added, size:",self.product_list.__len__());
 
     def lade_Listen(self):
         # Import Stations, flow and Products from XML
@@ -56,31 +56,29 @@ class Transport_Handler(object):
         if (False):
             pass
         else:
-            self.stations = [Station("Lager",[0,0]),
-                            Station("Trennen",[0,1]),
-                            Station("Bohren",[1,0]),
-                            Station("Fuegen",[3,2]),
-                            Station("Schweißen",[6,3]),
-                            Station("Polieren",[5,4]),
-                            Station("Ausgang",[6,6]),
-                            ];
+            self.stations = [Station("Lager", [0, 0]),
+                             Station("Trennen", [0, 1]),
+                             Station("Bohren", [1, 0]),
+                             Station("Fuegen", [3, 2]),
+                             Station("Schweißen", [6, 3]),
+                             Station("Polieren", [5, 4]),
+                             Station("Ausgang", [6, 6]),
+                             ];
             self.flow = [[0, 2],
-                        [1, 3],
-                        [2, 1],
-                        [4, 4],
-                        [3, 3],
-                        [5, 3],
-                        [6, 2]
+                         [1, 3],
+                         [2, 1],
+                         [4, 4],
+                         [3, 3],
+                         [5, 3],
+                         [6, 2]
                          ];
-            print("Keine XML gefunden, lade Ersatzwerte",self.flow)
+            print("Keine XML gefunden, lade Ersatzwerte", self.flow)
+
 
 ## Main
 def run():
     start_object = Transport_Handler();
-    start_object.start(100); # Anzahl der zu fertigenden Produkte
+    start_object.start(2);  # Anzahl der zu fertigenden Produkte
 
 
 run();
-
-
-
